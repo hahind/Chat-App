40ddda770c1f5d56a0e3174274a9271516a8361f
Matas Butvilas <matasbutvilas@gmail.com>
2025-03-18 03:53:06 +0100

    Entries to user ui

--- Ui.py
+++ Ui.py
@@ -1,10 +1,26 @@
 import tkinter as tk
 
 tkinter = tk.Tk()
-tkinter.title("Ui for user")
+tkinter.title("UI for User")
 tkinter.geometry("600x400")
 
-tkinter_label = tk.Label(tkinter, text="Test")
-tkinter_label.pack(padx=20, pady=20)
+username_label = tk.Label(tkinter, text="Username:")
+username_label.pack(padx=20, pady=(40,5))
 
-tkinter.mainloop()            
+username_entry = tk.Entry(tkinter, width=30)
+username_entry.pack(padx=20, pady=5)
+
+password_label = tk.Label(tkinter, text="Password:")
+password_label.pack(padx=20, pady=5)
+
+password_entry = tk.Entry(tkinter, show="*", width=30)
+password_entry.pack(padx=20, pady=5)
+def submitAction():
+    username = username_entry.get()
+    password = password_entry.get()
+    print(f"Username: {username}, Password: {password}")
+
+submit_button = tk.Button(tkinter, text="Submit", command=submitAction)
+submit_button.pack(padx=20, pady=20)
+
+tkinter.mainloop()
